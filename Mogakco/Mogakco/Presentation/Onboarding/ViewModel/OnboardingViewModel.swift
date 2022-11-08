295cb061d72c01e0ee6adad1a8e5b37827a96f1d
김태현 <taehyk528@gmail.com>
2022-11-08 23:47:45 +0900

    🌱 :: OnboardingViewModel 파일 추가(#19)

---
+++ Mogakco/Mogakco/Presentation/Onboarding/ViewModel/OnboardingViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingViewModel.swift
+//  Mogakco
+//
+//  Created by taekki on 2022/11/08.
+//
+
+import Foundation
+
+import RxSwift
+import RxCocoa
+
+final class OnboardingViewModel {
+    
+}
