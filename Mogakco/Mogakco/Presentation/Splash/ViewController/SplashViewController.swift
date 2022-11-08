431660ceb93a046637d766e02d520fb3a9ce7637
김태현 <taehyk528@gmail.com>
2022-11-08 13:29:40 +0900

    💄 :: 스플래시 뷰 레이아웃 구현(#9)

--- Mogakco/Mogakco/Presentation/Splash/ViewController/SplashViewController.swift
+++ Mogakco/Mogakco/Presentation/Splash/ViewController/SplashViewController.swift
@@ -7,11 +7,53 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 final class SplashViewController: BaseViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    private enum Metric {
+        static let logoTopMargin = 175.0
+        static let logoWidth = 220.0
+        static let logoHeight = 264.0
+        static let textTopMargin = 34.0
+        static let textWidth = 292.0
+        static let textHeight = 101.0
+    }
+    
+    private let splashLogoImageView = UIImageView()
+    private let splashTextImageView = UIImageView()
+    
+    override func setAttributes() {
+        view.backgroundColor = .systemBackground
         
-        view.backgroundColor = .green
+        splashLogoImageView.do {
+            $0.image = .imgSplashLogo
+        }
+        
+        splashTextImageView.do {
+            $0.image = .imgSplashText
+        }
+    }
+    
+    override func setHierarchy() {
+        view.addSubview(splashLogoImageView)
+        view.addSubview(splashTextImageView)
+    }
+    
+    override func setLayout() {
+        splashLogoImageView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(Metric.logoTopMargin)
+            $0.width.equalTo(Metric.logoWidth)
+            $0.height.equalTo(Metric.logoHeight)
+            $0.centerX.equalToSuperview()
+        }
+        
+        splashTextImageView.snp.makeConstraints {
+            $0.top.equalTo(splashLogoImageView.snp.bottom).offset(Metric.textTopMargin)
+            $0.width.equalTo(Metric.textWidth)
+            $0.height.equalTo(Metric.textHeight)
+            $0.centerX.equalTo(splashLogoImageView.snp.centerX)
+        }
     }
 }
