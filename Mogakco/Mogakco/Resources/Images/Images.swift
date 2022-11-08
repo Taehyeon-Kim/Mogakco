f61e0c2336b0d883ff27505c731054a187a8bce5
김태현 <taehyk528@gmail.com>
2022-11-08 13:24:24 +0900

    🍱 :: 스플래시 뷰 에셋 추가(#9)

--- Mogakco/Mogakco/Resources/Images/Images.swift
+++ Mogakco/Mogakco/Resources/Images/Images.swift
@@ -5,4 +5,27 @@
 //  Created by taekki on 2022/11/07.
 //
 
-import Foundation
+import UIKit
+
+extension UIImage {
+    
+    func resized(width: CGFloat, height: CGFloat) -> UIImage {
+        let size = CGSize(width: width, height: height)
+        let render = UIGraphicsImageRenderer(size: size)
+        let renderImage = render.image { _ in
+            self.draw(in: CGRect(origin: .zero, size: size))
+        }
+        return renderImage
+    }
+    
+    func resized(side: CGFloat) -> UIImage {
+        return self.resized(width: side, height: side)
+    }
+}
+
+extension UIImage {
+    
+    // Splash
+    static var imgSplashLogo: UIImage? { return UIImage(named: "splash_logo") }
+    static var imgSplashText: UIImage? { return UIImage(named: "txt") }
+}
