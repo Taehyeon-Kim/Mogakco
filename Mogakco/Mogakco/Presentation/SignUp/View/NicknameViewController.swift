2b5231868188bcd9cf086e529342b864e6b4d6ac
김태현 <taehyk528@gmail.com>
2022-11-10 23:06:36 +0900

    🎨 :: 닉네임 뷰, 이메일 뷰 코드 디테일 수정(#24)

--- Mogakco/Mogakco/Presentation/SignUp/View/NicknameViewController.swift
+++ Mogakco/Mogakco/Presentation/SignUp/View/NicknameViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 final class NicknameViewController: BaseViewController {
     
     private let textLabel = UILabel()
-    private let phoneEntryField = UITextField()
+    private let textField = UITextField()
     private let verificationCodeButton = SSButton(.disable)
     
     override func viewDidLoad() {
@@ -28,10 +28,9 @@ final class NicknameViewController: BaseViewController {
             $0.setTypoStyle(.display1, alignment: .center)
         }
         
-        phoneEntryField.do {
+        textField.do {
             $0.placeholder = "10자 이내로 입력"
             $0.font = .init(.regular, 14)
-            $0.keyboardType = .numberPad
             $0.setBottomBorder(with: .Gray.gray3, width: 1)
             $0.becomeFirstResponder()
         }
@@ -43,7 +42,7 @@ final class NicknameViewController: BaseViewController {
     
     override func setHierarchy() {
         view.addSubview(textLabel)
-        view.addSubview(phoneEntryField)
+        view.addSubview(textField)
         view.addSubview(verificationCodeButton)
     }
     
@@ -53,14 +52,14 @@ final class NicknameViewController: BaseViewController {
             $0.centerX.equalToSuperview()
         }
         
-        phoneEntryField.snp.makeConstraints {
+        textField.snp.makeConstraints {
             $0.top.equalTo(textLabel.snp.bottom).offset(77)
             $0.directionalHorizontalEdges.equalToSuperview().inset(16)
             $0.height.equalTo(47)
         }
         
         verificationCodeButton.snp.makeConstraints {
-            $0.top.equalTo(phoneEntryField.snp.bottom).offset(72)
+            $0.top.equalTo(textField.snp.bottom).offset(72)
             $0.directionalHorizontalEdges.equalToSuperview().inset(16)
             $0.height.equalTo(48)
         }
