d0e6c34724037884a8ce27b6632989c9b6d087c4
김태현 <taehyk528@gmail.com>
2022-11-10 22:57:15 +0900

    💄 :: 닉네임 뷰 레이아웃 가이드 구현(#24)

--- Mogakco/Mogakco/Presentation/SignUp/View/NicknameViewController.swift
+++ Mogakco/Mogakco/Presentation/SignUp/View/NicknameViewController.swift
@@ -9,8 +9,61 @@ import UIKit
 
 final class NicknameViewController: BaseViewController {
     
+    private let textLabel = UILabel()
+    private let phoneEntryField = UITextField()
+    private let verificationCodeButton = SSButton(.disable)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func setAttributes() {
+        view.backgroundColor = .Gray.white
+        
+        textLabel.do {
+            $0.text = """
+            닉네임을 입력해 주세요
+            """
+            $0.numberOfLines = 0
+            $0.setTypoStyle(.display1, alignment: .center)
+        }
+        
+        phoneEntryField.do {
+            $0.placeholder = "10자 이내로 입력"
+            $0.font = .init(.regular, 14)
+            $0.keyboardType = .numberPad
+            $0.setBottomBorder(with: .Gray.gray3, width: 1)
+            $0.becomeFirstResponder()
+        }
+        
+        verificationCodeButton.do {
+            $0.title = "다음"
+        }
+    }
+    
+    override func setHierarchy() {
+        view.addSubview(textLabel)
+        view.addSubview(phoneEntryField)
+        view.addSubview(verificationCodeButton)
+    }
+    
+    override func setLayout() {
+        textLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(125)
+            $0.centerX.equalToSuperview()
+        }
+        
+        phoneEntryField.snp.makeConstraints {
+            $0.top.equalTo(textLabel.snp.bottom).offset(77)
+            $0.directionalHorizontalEdges.equalToSuperview().inset(16)
+            $0.height.equalTo(47)
+        }
+        
+        verificationCodeButton.snp.makeConstraints {
+            $0.top.equalTo(phoneEntryField.snp.bottom).offset(72)
+            $0.directionalHorizontalEdges.equalToSuperview().inset(16)
+            $0.height.equalTo(48)
+        }
     }
 }
 
