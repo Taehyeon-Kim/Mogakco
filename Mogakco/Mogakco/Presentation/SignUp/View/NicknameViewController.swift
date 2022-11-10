3450e20ee7a0c660598735de82a75e167a907e9b
김태현 <taehyk528@gmail.com>
2022-11-10 22:54:34 +0900

    🌱 :: 회원가입 플로우 필요 파일 추가(#24)

---
+++ Mogakco/Mogakco/Presentation/SignUp/View/NicknameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NicknameViewController.swift
+//  Mogakco
+//
+//  Created by taekki on 2022/11/10.
+//
+
+import UIKit
+
+final class NicknameViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension NicknameViewController {
+    
+}
