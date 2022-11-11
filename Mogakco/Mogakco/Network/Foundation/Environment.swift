9b70b9eaa1a9225eec6c0d2c140aa71d381250f0
김태현 <taehyk528@gmail.com>
2022-11-11 19:14:04 +0900

    ✨ :: baseURL 추가(#28)

---
+++ Mogakco/Mogakco/Network/Foundation/Environment.swift
@@ -0,0 +1,21 @@
+//
+//  Environment.swift
+//  Mogakco
+//
+//  Created by taekki on 2022/11/11.
+//
+
+import Foundation
+
+struct Env {
+    
+    static var baseURL: String {
+        #if DEBUG
+        return "http://api.sesac.co.kr:1207"
+        #else
+        return ""
+        #endif
+    }
+    
+    static let apiVersion = "v1"
+}
