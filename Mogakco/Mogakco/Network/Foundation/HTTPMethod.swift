b8c5584a0ff9a22054faf6902fc2e14e9886d6f9
김태현 <taehyk528@gmail.com>
2022-11-11 18:54:06 +0900

    ✨ :: HTTPMethod 열거형 정의(#28)

---
+++ Mogakco/Mogakco/Network/Foundation/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  Mogakco
+//
+//  Created by taekki on 2022/11/11.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
