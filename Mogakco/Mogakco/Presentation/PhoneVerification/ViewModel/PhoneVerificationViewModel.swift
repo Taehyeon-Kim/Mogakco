a65dbd871393567bfaeef43501e5496e04752486
김태현 <taehyk528@gmail.com>
2022-11-12 02:51:59 +0900

    ✨ :: 인증하기 뷰 MVVM 구조 구현(#32)

--- Mogakco/Mogakco/Presentation/PhoneVerification/ViewModel/PhoneVerificationViewModel.swift
+++ Mogakco/Mogakco/Presentation/PhoneVerification/ViewModel/PhoneVerificationViewModel.swift
@@ -7,6 +7,39 @@
 
 import Foundation
 
-final class PhoneVerificationViewModel {
+import RxCocoa
+import RxSwift
+
+final class PhoneVerificationViewModel: ViewModelType {
     
+    var disposeBag = DisposeBag()
+
+    struct Input {
+        let verificationCode: ControlProperty<String>
+        let resendButtonTrigger: ControlEvent<Void>
+        let verifyButtonTrigger: ControlEvent<Void>
+    }
+    
+    struct Output {
+        let verificationCode: Driver<String>
+        let isEnabled: Driver<Bool>
+    }
+    
+    func transform(input: Input) -> Output {
+        let maxNumber = 6
+        
+        let verificationCode = input.verificationCode
+            .scan("") { prev, next in
+                return next.count > maxNumber ? prev : next
+            }
+        
+        let isEnabled = verificationCode
+            .map { $0.count == maxNumber }
+            .asDriver(onErrorJustReturn: false)
+            
+        return Output(
+            verificationCode: verificationCode.asDriver(onErrorJustReturn: ""),
+            isEnabled: isEnabled
+        )
+    }
 }
