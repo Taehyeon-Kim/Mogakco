1658568b45cd43748c6d389b6962d0e1f493dc70
김태현 <taehyk528@gmail.com>
2022-11-11 21:22:09 +0900

    💄 :: PhoneEntryViewController UI 코드 수정(#32)

--- Mogakco/Mogakco/Presentation/PhoneVerification/View/PhoneEntryViewController.swift
+++ Mogakco/Mogakco/Presentation/PhoneVerification/View/PhoneEntryViewController.swift
@@ -9,65 +9,82 @@ import UIKit
 
 final class PhoneEntryViewController: BaseViewController {
     
-    private let textLabel = UILabel()
+    // MARK: Constants
+    
+    enum Metric {
+        static let padding = 16.adjustedWidth
+        static let textFieldLeftPadding = 12.0
+        
+        static let titleLabelTop = 125.adjustedHeight
+        
+        static let phoneEntryFieldTop = 65.adjustedHeight
+        static let phoneEntryFieldHeight = 47.adjustedHeight
+        
+        static let verificationCodeButtonTop = 72.adjustedHeight
+        static let verificationCodeButtonHeight = 48.adjustedHeight
+    }
+    
+    enum Text {
+        static let titleLabel = """
+        새싹 서비스 이용을 위해
+        휴대폰 번호를 입력해주세요
+        """
+        
+        static let phoneEntryField = "휴대폰 번호(-없이 숫자만 입력)"
+        static let verificationCodeButtonTitle = "인증 문자 받기"
+    }
+    
+    // MARK: UI
+    
+    private let titleLabel = UILabel()
     private let phoneEntryField = UITextField()
     private let verificationCodeButton = SSButton(.disable)
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
+    // MARK: Override Methods
+   
     override func setAttributes() {
         view.backgroundColor = .Gray.white
         
-        textLabel.do {
-            $0.text = """
-            새싹 서비스 이용을 위해
-            휴대폰 번호를 입력해주세요
-            """
+        titleLabel.do {
             $0.numberOfLines = 0
+            $0.text = Text.titleLabel
             $0.setTypoStyle(.display1, alignment: .center)
         }
         
         phoneEntryField.do {
-            $0.placeholder = "휴대폰 번호(-없이 숫자만 입력)"
+            $0.placeholder = Text.phoneEntryField
             $0.font = .init(.regular, 14)
             $0.keyboardType = .numberPad
             $0.setBottomBorder(with: .Gray.gray3, width: 1)
+            $0.setLeftPadding(to: Metric.textFieldLeftPadding)
             $0.becomeFirstResponder()
         }
         
         verificationCodeButton.do {
-            $0.title = "인증 문자 받기"
+            $0.title = Text.verificationCodeButtonTitle
         }
     }
     
     override func setHierarchy() {
-        view.addSubview(textLabel)
-        view.addSubview(phoneEntryField)
-        view.addSubview(verificationCodeButton)
+        view.addSubviews(titleLabel, phoneEntryField, verificationCodeButton)
     }
     
     override func setLayout() {
-        textLabel.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide).offset(125)
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(Metric.titleLabelTop)
             $0.centerX.equalToSuperview()
         }
         
         phoneEntryField.snp.makeConstraints {
-            $0.top.equalTo(textLabel.snp.bottom).offset(77)
-            $0.directionalHorizontalEdges.equalToSuperview().inset(16)
-            $0.height.equalTo(47)
+            $0.top.equalTo(titleLabel.snp.bottom).offset(Metric.phoneEntryFieldTop)
+            $0.directionalHorizontalEdges.equalToSuperview().inset(Metric.padding)
+            $0.height.equalTo(Metric.phoneEntryFieldHeight)
         }
         
         verificationCodeButton.snp.makeConstraints {
-            $0.top.equalTo(phoneEntryField.snp.bottom).offset(72)
-            $0.directionalHorizontalEdges.equalToSuperview().inset(16)
-            $0.height.equalTo(48)
+            $0.top.equalTo(phoneEntryField.snp.bottom).offset(Metric.verificationCodeButtonTop)
+            $0.directionalHorizontalEdges.equalToSuperview().inset(Metric.padding)
+            $0.height.equalTo(Metric.verificationCodeButtonHeight)
         }
     }
-}
-
-extension PhoneEntryViewController {
-    
 }
