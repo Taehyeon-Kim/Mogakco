25123388ddc41513fc548e10783d552709ba18a0
김태현 <taehyk528@gmail.com>
2022-11-13 02:36:30 +0900

    ✨ :: 닉네임 뷰 동작 구현(#38)

--- Mogakco/Mogakco/Presentation/SignUp/View/NicknameViewController.swift
+++ Mogakco/Mogakco/Presentation/SignUp/View/NicknameViewController.swift
@@ -7,14 +7,34 @@
 
 import UIKit
 
+import RxCocoa
+import RxSwift
+
 final class NicknameViewController: BaseViewController {
+    
+    // MARK: UI
     
     private let textLabel = UILabel()
     private let textField = UITextField()
-    private let verificationCodeButton = SSButton(.disable)
+    private let nextButton = SSButton(.disable)
+    
+    // MARK: Properties
+    
+    var viewModel: NicknameViewModel
+    
+    // MARK: Initializing
+    
+    init(viewModel: NicknameViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        bind()
     }
     
     override func setAttributes() {
@@ -36,7 +56,7 @@ final class NicknameViewController: BaseViewController {
             $0.becomeFirstResponder()
         }
         
-        verificationCodeButton.do {
+        nextButton.do {
             $0.title = "다음"
         }
     }
@@ -44,7 +64,7 @@ final class NicknameViewController: BaseViewController {
     override func setHierarchy() {
         view.addSubview(textLabel)
         view.addSubview(textField)
-        view.addSubview(verificationCodeButton)
+        view.addSubview(nextButton)
     }
     
     override func setLayout() {
@@ -59,7 +79,7 @@ final class NicknameViewController: BaseViewController {
             $0.height.equalTo(47)
         }
         
-        verificationCodeButton.snp.makeConstraints {
+        nextButton.snp.makeConstraints {
             $0.top.equalTo(textField.snp.bottom).offset(72)
             $0.directionalHorizontalEdges.equalToSuperview().inset(16)
             $0.height.equalTo(48)
@@ -67,6 +87,38 @@ final class NicknameViewController: BaseViewController {
     }
 }
 
+extension NicknameViewController: Bindable {
+    
+    func bind() {
+        let input = NicknameViewModel.Input(
+            changedText: textField.rx.text.orEmpty,
+            nextButtonTrigger: nextButton.button.rx.tap
+        )
+        let output = viewModel.transform(input: input)
+        
+        output.nickname
+            .drive(textField.rx.text)
+            .disposed(by: disposeBag)
+        
+        output.isEnabled
+            .withUnretained(self)
+            .emit { `self`, isEnabled in
+                self.nextButton.buttonStyle = isEnabled ? .fill : .disable
+            }
+            .disposed(by: disposeBag)
+        
+        output.nextButtonTrigger
+            .drive { [weak self] _ in
+                let viewController = BirthViewController()
+                self?.transition(to: viewController)
+            }
+            .disposed(by: disposeBag)
+    }
+}
+
 extension NicknameViewController {
     
+    private func transition(to viewController: UIViewController) {
+        navigationController?.pushViewController(viewController, animated: true)
+    }
 }
