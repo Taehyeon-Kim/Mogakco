ee554d814d6ee18c6c84c3b8068950a98d3f5d44
김태현 <taehyk528@gmail.com>
2022-11-15 02:39:40 +0900

    ✨ :: SignUpRequestDTO 구현(#44)

---
+++ Mogakco/Mogakco/DataMapping/SignUp/SignUpRequestDTO.swift
@@ -0,0 +1,31 @@
+//
+//  SignUpRequestDTO.swift
+//  Mogakco
+//
+//  Created by taekki on 2022/11/15.
+//
+
+import Foundation
+
+// {
+//   "phoneNumber" : "+821012345678",
+//   "FCMtoken" : "dzjnejNDh0d0u1aLzfS547:APA91bFvQSjDVFC4-2IA0QQ08KqsEKwIoK2hFBZIfdyNLPd22PvgLD6YM_kyQgv0BIK-1zjltbbKYQTGK50Pn21bctsuEC46qo7RDkcImbzyZBe0-ffMqhFhL4DO5tbP0Ri_Wn-vRVF5",
+//   "nick": "고래밥",
+//   "birth": "2002-01-16T09:23:44.054Z",
+//   "email": "user@example.com",
+//   "gender" : 0
+// }
+
+struct SignUpRequestDTO: Encodable {
+    var phoneNumber: String
+    var fcmToken: String
+    var nick: String
+    var birth: String
+    var email: String
+    var gender: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case phoneNumber, nick, birth, email, gender
+        case fcmToken = "FCMtoken"
+    }
+}
