f7808828415b14c00c0bbb01492fa40fc80644d8
김태현 <taehyk528@gmail.com>
2022-11-14 22:18:17 +0900

    🌱 :: PreviewProvider 추가(#39)

---
+++ Mogakco/Mogakco/Utils/Preview/Device.swift
@@ -0,0 +1,15 @@
+//
+//  Device.swift
+//  Mogakco
+//
+//  Created by taekki on 2022/11/14.
+//
+
+import Foundation
+
+public enum Device: String {
+    case iPhone8        = "iPhone 8"
+    case iPhone13Mini   = "iPhone 13 mini"
+    case iPhone13ProMax = "iPhone 13 Pro max"
+    case iPhone14Pro    = "iPhone 14 Pro"
+}
