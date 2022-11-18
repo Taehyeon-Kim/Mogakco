b26857345bb83a29142973d10e74003e5e0f0d26
김태현 <taehyk528@gmail.com>
2022-11-18 19:14:30 +0900

    🌱 :: AuthManager 구현, renewalToken 메서드 추가(#46)

---
+++ Mogakco/Mogakco/Services/Auth/AuthManager.swift
@@ -0,0 +1,44 @@
+//
+//  AuthManager.swift
+//  Mogakco
+//
+//  Created by taekki on 2022/11/18.
+//
+
+import Foundation
+
+import FirebaseAuth
+import RxSwift
+
+protocol AuthManager {
+    func renewalToken() -> Single<String>
+}
+
+final class AuthManagerImpl: AuthManager {
+    
+    private let auth: Auth
+    
+    init(auth: Auth = Auth.auth()) {
+        self.auth = auth
+    }
+    
+    func renewalToken() -> Single<String> {
+        
+        return Single.create { [unowned self] observer in
+            
+            auth.currentUser?.getIDTokenForcingRefresh(true) { token, error in
+                
+                if let error = error {
+                    observer(.failure(error))
+                }
+                
+                if let token = token {
+                    print("♻️ 토큰 갱신: \(token)")
+                    observer(.success(token))
+                }
+            }
+            
+            return Disposables.create()
+        }
+    }
+}
