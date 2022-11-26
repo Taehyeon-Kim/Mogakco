1f182cbbff77df02ee182daaaeb4837468845bd5
김태현 <taehyk528@gmail.com>
2022-11-26 19:42:44 +0900

    ✨ :: KeyboardView 키보드 동작에 따른 뷰 동적 조정(#59)

--- Mogakco/Mogakco/Presentation/StudyKeyword/View/KeywordViewController.swift
+++ Mogakco/Mogakco/Presentation/StudyKeyword/View/KeywordViewController.swift
@@ -7,15 +7,70 @@
 
 import UIKit
 
+import RxCocoa
+import RxGesture
+import RxSwift
+import RxKeyboard
+
 final class KeywordViewController: BaseViewController {
     
     private let rootView = KeywordView()
+    private let viewModel = KeywordViewModel()
     
     override func loadView() {
         self.view = rootView
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bind()
+    }
 }
 
-extension KeywordViewController {
+extension KeywordViewController: Bindable {
     
+    func bind() {
+        let input = KeywordViewModel.Input(
+            searchText: rootView.searchBar.rx.text,
+            textDidBeginEditing: rootView.searchBar.rx.textDidBeginEditing
+        )
+        let output = viewModel.transform(input: input)
+
+        output.wantedList
+            .bind { keywords in
+                print(keywords)
+            }
+            .disposed(by: disposeBag)
+        
+        output.textDidBeginEditing
+            .bind { _ in
+                
+            }
+            .disposed(by: disposeBag)
+        
+        RxKeyboard.instance.visibleHeight
+            .drive(onNext: { [unowned self] keyboardHeight in
+                let height: CGFloat = keyboardHeight > 0 ? -keyboardHeight + view.safeAreaInsets.bottom : -16
+                let padding: CGFloat = keyboardHeight > 0 ? 0 : 16
+                let radius: CGFloat = keyboardHeight > 0 ? 0 : 8
+
+                rootView.collectionView.contentInset.bottom = keyboardHeight
+                rootView.collectionView.contentOffset.y += keyboardHeight
+                rootView.findButton.makeRounded(radius: radius)
+                rootView.findButton.snp.updateConstraints {
+                    $0.directionalHorizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(padding)
+                    $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(height)
+                }
+
+                view.layoutIfNeeded()
+            })
+        .disposed(by: disposeBag)
+        
+        view.rx.tapGesture()
+            .when(.recognized)
+            .subscribe { [weak self] _ in
+                self?.view.endEditing(true)
+            }
+            .disposed(by: disposeBag)
+    }
 }
