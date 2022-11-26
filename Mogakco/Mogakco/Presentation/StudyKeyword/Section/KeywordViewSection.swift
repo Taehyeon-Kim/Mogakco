ee7025a71c16a5ad1d58a54a53ae747c4dcbcd13
김태현 <taehyk528@gmail.com>
2022-11-26 15:32:14 +0900

    🌱 :: StudyKeyword 파일 계층 구성(#59)

---
+++ Mogakco/Mogakco/Presentation/StudyKeyword/Section/KeywordViewSection.swift
@@ -0,0 +1,13 @@
+//
+//  KeywordViewSection.swift
+//  Mogakco
+//
+//  Created by taekki on 2022/11/26.
+//
+
+import Foundation
+
+enum KeywordViewSection {
+    case recommended
+    case wanted
+}
---
+++ Mogakco/Mogakco/Presentation/StudyKeyword/View/KeywordViewController.swift
@@ -0,0 +1,21 @@
+//
+//  KeywordViewController.swift
+//  Mogakco
+//
+//  Created by taekki on 2022/11/26.
+//
+
+import UIKit
+
+final class KeywordViewController: BaseViewController {
+    
+    private let rootView = KeywordView()
+    
+    override func loadView() {
+        self.view = rootView
+    }
+}
+
+extension KeywordViewController {
+    
+}
