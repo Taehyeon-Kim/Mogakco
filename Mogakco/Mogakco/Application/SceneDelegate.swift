389a44ac7e2c8625561dd9b2ec5006f68126174b
김태현 <taehyk528@gmail.com>
2022-11-26 15:12:11 +0900

    ✨ :: TabBarController 구현(#57)

--- Mogakco/Mogakco/Application/SceneDelegate.swift
+++ Mogakco/Mogakco/Application/SceneDelegate.swift
@@ -15,9 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
 
-        
-        let viewModel = MapViewModel()
-        window?.rootViewController = MapViewController(viewModel: viewModel)
+        window?.rootViewController = TabBarController()
         window?.makeKeyAndVisible()
     }
 }
---
+++ Mogakco/Mogakco/Presentation/Friend/FriendViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FriendViewController.swift
+//  Mogakco
+//
+//  Created by taekki on 2022/11/26.
+//
+
+import UIKit
+
+final class FriendViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension FriendViewController {
+    
+}
