f5a4625e1abdecaafe8b6c754fe4398b92458fe1
김태현 <taehyk528@gmail.com>
2022-11-26 17:24:49 +0900

    ✨ :: KeywordView 레이아웃 구현(#59)

--- Mogakco/Mogakco/Presentation/StudyKeyword/Section/KeywordViewSection.swift
+++ Mogakco/Mogakco/Presentation/StudyKeyword/Section/KeywordViewSection.swift
@@ -7,7 +7,19 @@
 
 import Foundation
 
-enum KeywordViewSection {
+enum KeywordViewSection: CaseIterable {
     case recommended
     case wanted
+}
+
+extension KeywordViewSection {
+    
+    var title: String {
+        switch self {
+        case .recommended:
+            return "지금 주변에는"
+        case .wanted:
+            return "내가 하고 싶은"
+        }
+    }
 }
