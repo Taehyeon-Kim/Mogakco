e2f95c9293ce86caf4131f4465831e30706604ac
김태현 <taehyk528@gmail.com>
2022-11-28 19:57:47 +0900

    ✨ :: Pager 적용(#63)

---
+++ Mogakco/Mogakco/Presentation/StudyMatch/View/StudyAcceptViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StudyAcceptViewController.swift
+//  Mogakco
+//
+//  Created by taekki on 2022/11/28.
+//
+
+import UIKit
+
+final class StudyAcceptViewController: BaseViewController, PageComponentProtocol {
+    
+    var pageTitle = "받은 요청"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // view.backgroundColor = .blue
+    }
+}
---
+++ Mogakco/Mogakco/Presentation/StudyMatch/View/StudyRequestViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StudyRequestViewController.swift
+//  Mogakco
+//
+//  Created by taekki on 2022/11/28.
+//
+
+import UIKit
+
+final class StudyRequestViewController: BaseViewController, PageComponentProtocol {
+    
+    var pageTitle = "주변 새싹"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // view.backgroundColor = .red
+    }
+}
