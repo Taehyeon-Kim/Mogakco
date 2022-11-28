d8b129d1b5f48bd8fc59b1dc59b3713df4c499e9
김태현 <taehyk528@gmail.com>
2022-11-28 21:49:09 +0900

    ✨ :: 커스텀 팝업 구현 aka. MGCPopUp(#63)

--- Mogakco/Mogakco/Presentation/StudyMatch/View/MatchViewController.swift
+++ Mogakco/Mogakco/Presentation/StudyMatch/View/MatchViewController.swift
@@ -12,10 +12,41 @@ import Then
 
 final class MatchViewController: BaseViewController {
     
+    enum Literal {
+        static let popUpTitle = "스터디를 취소하겠습니까?"
+        static let popUpDesc = """
+        스터디를 취소하시면 패널티가 부과됩니다
+        """
+    }
+    
     private let pager = Pager()
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // PopUp을 Window에 추가해주고 있기 때문에
+    // 뷰가 완전히 로드된 이후에 띄워주어야 한다.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        MGCPopUp.shared
+            .setType(.cancelStudy)
+            .present { type in
+            switch type {
+            case .cancel:
+                print("")
+                // 취소 액션
+                
+            case .confirm:
+                print("")
+                // 확인 액션
+            }
+        }
+    }
+    
     override func setHierarchy() {
-        view.addSubview(pager)
+        view.addSubviews(pager)
     }
     
     override func setLayout() {
