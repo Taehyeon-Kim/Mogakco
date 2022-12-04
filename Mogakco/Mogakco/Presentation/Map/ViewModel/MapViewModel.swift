a51d44064501700759c8564182b39bbbbf769365
김태현 <taehyk528@gmail.com>
2022-12-05 01:17:41 +0900

    ✨ :: Pin Custom Annotation, Check Queue State(#73)

--- Mogakco/Mogakco/Presentation/Map/ViewModel/MapViewModel.swift
+++ Mogakco/Mogakco/Presentation/Map/ViewModel/MapViewModel.swift
@@ -13,10 +13,19 @@ import RxSwift
 final class MapViewModel: ViewModelType {
 
     private var disposeBag = DisposeBag()
-    private let locationManager: LocationManager
-    private let location = PublishRelay<CLLocation>()
     
-    init(locationManager: LocationManager) {
+    private let networkProvider: NetworkProvider
+    private let locationManager: LocationManager
+    
+    private let location = PublishRelay<CLLocation>()
+    private let authorization = PublishRelay<CLAuthorizationStatus>()
+    private let queueStatue = BehaviorRelay<QueueState>(value: .general)
+    
+    init(
+        networkProvider: NetworkProvider,
+        locationManager: LocationManager
+    ) {
+        self.networkProvider = networkProvider
         self.locationManager = locationManager
     }
     
@@ -24,11 +33,14 @@ final class MapViewModel: ViewModelType {
         let viewDidAppear: Observable<Void>
         let locationButtonDidTap: Observable<Void>
         let centerLocation: BehaviorRelay<CLLocationCoordinate2D>
+        let floatingButtonDidTap: Observable<Void>
     }
     
     struct Output {
         let centerLocation = BehaviorRelay<CLLocation>(value: CLLocation())
         let shouldMoveCenter = BehaviorRelay<Bool>(value: true)
+        let isfloatingButtonSelected = PublishRelay<CLAuthorizationStatus>()
+        let myQueueStatus = BehaviorRelay<QueueState>(value: .general)
     }
     
     func transform(input: Input) -> Output {
@@ -38,6 +50,7 @@ final class MapViewModel: ViewModelType {
             .subscribe(with: self) { owner, _ in
                 owner.locationManager.startUpdatingLocation()
                 owner.requestLocation()
+                owner.checkMyQueueState()
             }
             .disposed(by: disposeBag)
         
@@ -51,6 +64,18 @@ final class MapViewModel: ViewModelType {
             .bind(to: output.centerLocation)
             .disposed(by: disposeBag)
         
+        input.floatingButtonDidTap
+            .withUnretained(self)
+            .flatMap { owner, _ in owner.locationManager.observeAuthorization() }
+            .subscribe(with: self) { owner, status in
+                owner.authorization.accept(status)
+            }
+            .disposed(by: disposeBag)
+        
+        output.myQueueStatus
+            .bind(to: queueStatue)
+            .disposed(by: disposeBag)
+        
         return output
     }
 }
@@ -62,6 +87,22 @@ extension MapViewModel {
             .compactMap { $0.last }
             .subscribe(with: self) { owner, location in
                 owner.location.accept(location)
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    func checkMyQueueState() {
+        let myQueueStateAPI = MyQueueStateAPI()
+        networkProvider.execute(of: myQueueStateAPI)
+            .subscribe(with: self) { owner, queueStateDTO in
+                if queueStateDTO.matched == 0 {
+                    owner.queueStatue.accept(.waiting)
+                } else if queueStateDTO.matched == 1 {
+                    owner.queueStatue.accept(.matched)
+                }
+    
+            } onFailure: { owner, error in
+                owner.queueStatue.accept(.general)
             }
             .disposed(by: disposeBag)
     }
