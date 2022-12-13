7c82ac9e548944b694f94c23298a7af45fe58a24
김태현 <taehyk528@gmail.com>
2022-12-13 19:38:25 +0900

    ♻️  :: EmailViewController 리팩토링(#83)

--- Mogakco/Mogakco/Presentation/SignUp/ViewModel/EmailViewModel.swift
+++ Mogakco/Mogakco/Presentation/SignUp/ViewModel/EmailViewModel.swift
@@ -13,10 +13,10 @@ import RxSwift
 final class EmailViewModel: ViewModelType {
 
     private var disposeBag = DisposeBag()
-    private var email = "asdfasdfasd@asdfa.com"
-    
     private var validator: Validator
     private var userManager: UserManager
+    
+    private var email = BehaviorRelay(value: "")
     
     init(
         validator: Validator,
@@ -27,31 +27,44 @@ final class EmailViewModel: ViewModelType {
     }
 
     struct Input {
-        let changedText: ControlProperty<String>
-        let nextButtonTrigger: ControlEvent<Void>
+        let emailValue: Observable<String>
+        let nextButtonTrigger: Observable<Void>
     }
     
     struct Output {
-        let isEnabled: Signal<Bool>
+        let isEnabled: Driver<Bool>
         let nextButtonTrigger: Driver<Void>
+        let success: Driver<Void>
+        let error: Driver<String>
     }
     
     func transform(input: Input) -> Output {
         
-        let isEnabled = input.changedText
-            .asSignal(onErrorJustReturn: "")
+        let isEnabled = input.emailValue
+            .asDriver(onErrorJustReturn: "")
             .map(validator.isValid(email:))
         
+        let success = PublishRelay<Void>()
+        let error = PublishRelay<String>()
+        
+        input.nextButtonTrigger
+            .withLatestFrom(isEnabled)
+            .asDriver(onErrorJustReturn: false)
+            .drive(with: self) { owner, isEnabled in
+                if isEnabled {
+                    owner.userManager.store(email: owner.email.value)
+                    success.accept(())
+                } else {
+                    error.accept("이메일 형식이 올바르지 않습니다.")
+                }
+            }
+            .disposed(by: disposeBag)
+            
         return Output(
             isEnabled: isEnabled,
-            nextButtonTrigger: input.nextButtonTrigger.asDriver()
+            nextButtonTrigger: input.nextButtonTrigger.asDriver(onErrorJustReturn: ()),
+            success: success.asDriver(onErrorJustReturn: ()),
+            error: error.asDriver(onErrorJustReturn: "")
         )
-    }
-}
-
-extension EmailViewModel {
-    
-    func storeEmail() {
-        userManager.store(email: self.email)
     }
 }
